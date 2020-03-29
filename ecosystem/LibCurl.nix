3bbb2d69489593e506a5faf683b685db0519a845
Chloé <rightfold+ck@gmail.com>
2020-03-29 20:31:37 +0200

    LibCurl: Suppress warning about $HOME missing

--- ecosystem/LibCurl.nix
+++ ecosystem/LibCurl.nix
@@ -9,6 +9,10 @@ rakuPackage {
         sha256 = "02f5sa3dl1xfmywshqn2883k6wh8r2zngbmiphjafznqcybd5n17";
     };
     preInstallPhase = ''
+        # This package likes to use HOME during compilation.
+        mkdir home
+        export HOME=$PWD/home
+
         # This package likes to use libcurl during compilation.
         ldLibraryPath=${lib.makeLibraryPath [curl]}
         export LD_LIBRARY_PATH=$ldLibraryPath:$LD_LIBRARY_PATH
