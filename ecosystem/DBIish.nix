33a06629e6e50cb075c035c0209caf3787c5612e
Chloé <rightfold+ck@gmail.com>
2020-04-02 16:54:58 +0200

    bla

--- ecosystem/DBIish.nix
+++ ecosystem/DBIish.nix
@@ -3,7 +3,7 @@ rakuPackage {
     name = "DBIish";
     depends = [rakuPackages.NativeHelpers-Blob];
     src = fetchTarball {
-        url = "https://github.com/perl6/DBIish/archive/d89c8c842e467c26f679cb65913d2fe9aded1a6d.tar.gz";
-        sha256 = "11hvjjx9ghq01mx6q2ar6h2mc7vskhmyl4472hzfqa48xvdvjnm4";
+        url = "https://github.com/perl6/DBIish/archive/c21b2deb62509e08c0ee9db9a5213b18dd2b932d.tar.gz";
+        sha256 = "1ivgn06wdxld0kplx2ilywwbgxj2cimxxnmj2p6kkcnzsk3p1fzr";
     };
 }
